36127bee80c64026a9de740b4da71f8f757e7920
kkuratolodon <mif05123@gmail.com>
2024-02-14 14:16:42 +0700

    dealing with issues

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -13,8 +13,12 @@ import java.util.NoSuchElementException;
 @Service
 public class ProductServiceImpl implements  ProductService {
 
+    private final ProductRepository productRepository;
+
     @Autowired
-    private ProductRepository productRepository;
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
     public Product create(Product product) {
