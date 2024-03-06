b0163e5a599855a49c64aaf99a92cefaec8b0f56
kkuratolodon <mif05123@gmail.com>
2024-03-06 19:47:10 +0700

    [RED] Add PaymentByBankTransfer skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByBankTransfer.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class PaymentByBankTransfer extends Payment {
+    public PaymentByBankTransfer(String id, Order order, String method, Map<String, String> paymentData, String status) {
+    }
+
+    public PaymentByBankTransfer(String id, Order order, String method, Map<String, String >paymentData) {
+    }
+}
