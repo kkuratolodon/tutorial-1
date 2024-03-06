c2cbe9ebed26172b97d69772b098de0ad9c59f6b
kkuratolodon <mif05123@gmail.com>
2024-03-06 19:35:00 +0700

    [RED] Add PaymentByVoucher skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class PaymentByVoucher extends Payment {
+    public PaymentByVoucher(String id, Order order, String method, Map<String, String> paymentData, String status) {
+    }
+
+    public PaymentByVoucher(String id, Order order, String method, Map<String, String >paymentData) {
+    }
+}
